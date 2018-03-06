a366ae05aec35a266328c13b8cc99c307a9a67db
yeslem <my.abbad@gmail.com>
2018-03-06 18:19:19 +0800

    Removed unnecessary directory

--- OLD/fyp/app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url, include
-from app.views import *
-
-
-urlpatterns = [
-    url(r'^index/', IndexView.as_view(), name='index'),
-]
--- OLD/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\user\fyp\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
